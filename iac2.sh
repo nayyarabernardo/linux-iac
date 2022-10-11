5781814ecf5f01bfab0c976e199122b733c12276
nayyarabernardo <nayyarabernardo@gmail.com>
2022-10-10 23:42:46 -0300

    Apache v.1.0

---
+++ iac2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Atualizando o servidor.."
+apt-get update
+apt-get upgrade -y
+upt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando e copiando os arquivos da aplicação..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/maiin.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+
+
